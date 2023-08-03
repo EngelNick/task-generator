cacec254c8ebc718f6c7b6af710fda448a10be9e
Nikolay <zsnikolay@gmail.com>
2023-08-03 16:51:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function subscribeToInput() {
 }
 
 function createTask() {
-    const newTaskName = document.getElementById("input").value.split(' ').join('-').toLowerCase();
-    const replaceParenthesesAndSlashes = newTaskName.replace(/[()\s]/g, '').replace(/[/\s]/g, '-');
+    const newTaskName = document.getElementById("input").value.split(' ').join('_').toLowerCase();
+    const replaceParenthesesAndSlashes = newTaskName.replace(/[()\s]/g, '').replace(/[/\s]/g, '_');
     document.getElementById('result').innerHTML = replaceParenthesesAndSlashes ? replaceParenthesesAndSlashes : 'result';
 }
